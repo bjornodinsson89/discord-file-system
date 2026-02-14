37b11c696cd0079748cbae518178676d98459ccb
BjornOdinsson89 <bjornodinsson89@gmail.com>
2026-02-13 20:00:28 -0700

    Delete migrations directory

--- migrations/001_add_raffles_prize_image_url.sql
+++
@@ -1,2 +0,0 @@
--- Adds optional external prize image URL for raffle purchase embeds.
-ALTER TABLE raffles ADD COLUMN IF NOT EXISTS prize_image_url TEXT;
--- migrations/004_add_scheduled_start_text.sql
+++
@@ -1,3 +0,0 @@
--- Adds scheduled_start_text for older deployments where jump_99k_sessions was created without it.
-ALTER TABLE jump_99k_sessions
-    ADD COLUMN IF NOT EXISTS scheduled_start_text TEXT;
